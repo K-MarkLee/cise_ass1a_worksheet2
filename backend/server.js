55832dbf20b0d6936bc9cdd0b1b67619e2734f7a
승열 이 <vulca123@naver.com>
2021-08-02 21:04:40 +1200

    replace

--- backend/server.js
+++ backend/server.js
@@ -11,12 +11,12 @@ server.get('/api/articles', (req,res) => {
     res.json(articles);
 });
 
-//// id로 찾기
-// server.get('/api/articles/:id', (req,res) => {
-//     const article = articles.find((n) => n._id === req.params.id);
-//     res.send(article);
-//     console.log(req.params);
-// });
+// id로 찾기
+server.get('/api/articles/:id', (req,res) => {
+    const article = articles.find((n) => n._id === req.params.id);
+    res.send(article);
+    console.log(req.params);
+});
 const dotenv = require("dotenv");
 dotenv.config();
 const PORT = process.env.PORT || 5000;
